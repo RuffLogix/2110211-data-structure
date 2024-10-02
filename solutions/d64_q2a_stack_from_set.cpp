05f04ca14b379addfb13e3073af134019f2dfc2b
Teejuta Sriwaranon <teejuta.sriwaranon@gmail.com>
2024-10-02 17:02:08 +0700

    add: d64_q2a_stack_from_set.cpp

---
+++ solutions/d64_q2a_stack_from_set.cpp
@@ -0,0 +1,26 @@
+#ifndef __STACK_STUDENT_H__
+#define __STACK_STUDENT_H__
+#include "stack.h"
+
+//DO NOT INCLUDE ANYTHING
+
+
+template <typename T>
+CP::stack<T>::stack(typename std::set<T>::iterator first,typename std::set<T>::iterator last) {
+    int cap = 1;
+    mData = new T[cap]();
+    mCap = cap;
+    mSize = 0;
+    CP::stack<T> tmp;
+    while (first != last) {
+        tmp.push(*first);
+        first++;
+    }
+
+    while (!tmp.empty()) {
+        push(tmp.top());
+        tmp.pop();
+    }
+}
+
+#endif
